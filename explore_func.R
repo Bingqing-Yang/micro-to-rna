6200a913456a28fa9263a3d0ddf2c693dbeb412c
Bingqing Yang <yangbingqing225@163.com>
2023-09-25 11:07:15 +0800

    subfunction of explore

--- explore_func.R
+++ explore_func.R
@@ -20,21 +20,6 @@ library(scam)
 }
 
 
-.lm_gene <- function(gene) {
-  lm(rseqt[gene, ] ~ marrt[gene, ])
-}
-
-
-# Automatically generate scatter plots according to pic_num and probe_list
-scatter_plot <- function( pic_num, non_func_list) {
-  stopifnot(pic_num > 0 & pic_num < dim(non_func_list)[1])
-  for (i in 1:pic_num) {
-    print(.plot_gene(non_func_list$gene[i]))
-    readline("Press Enter to continue...")
-  }
-}
-
-
 
 # Calculate variance explained of first component by PCA for each probes
 pca.ratio <- function(mar ,rna ){
@@ -57,7 +42,7 @@ fev.func<- function(x, x.hat) {
 }
 
 
-# Calculate mutualinformation for each probes between Microarray and RNA-seq
+# Calculate mutual information for each probes between Microarray and RNA-seq
 mutual_info <- function(mar = marrt, rna = rseqt , meth = "equalfreq"){
   
   mar.T <- t(mar)
@@ -73,136 +58,228 @@ mutual_info <- function(mar = marrt, rna = rseqt , meth = "equalfreq"){
   return(mutual_lst)
 }
 
+
 # Calculate fev, linear regression, scatter plots and named the picture
-linear.scatter <- function(mar, rna, probes, func.lin, folderpath, num.pic, label){
+lin.scatter <- function(mar, rna, probes, func.lin, folderpath, num.pic , label, level, view, press){
   
+  # folder of picture
   if (!file.exists(folderpath)) {
     dir.create(folderpath)
   } else {
     cat("The filepath already exists\n")
   }
   
-  fev.lst <- c()
-  stopifnot(num.pic < dim(mar)[1])
-  if (missing(num.pic)) {num.pic = dim(mar)[1]} else { num.pic = num.pic}
+  # number pictures
+  if (missing(num.pic)) {num.pic = dim(mar)[1]; press = FALSE} else { num.pic = num.pic;  press = TRUE}
+  stopifnot(num.pic <= dim(mar)[1])
   
+  fev.lst <- c()
   for (i in 1:(num.pic)) {
-    # par(mfrow=c(1,2))
-    fev <- lin.scatter(i, mar, rna, probes)
-    fev.lst <- append(fev.lst, fev)
-    if(label == TRUE){
+    # Fit liner model and Prediction: PI
+    lin.pred <- linear(i, x = mar, y = rna, alpha = level)
+    
+    # fev
+    fev.hat <- fev.func(rna[i, ], lin.pred$model$fitted.values);
+    fev.lst <- append(fev.lst, fev.hat)
+
+    # Scatter plot with PI
+    scatter(i, mar, rna,  probes, pred = lin.pred$pred)
+
+    # Picture by picture.
+    if (press == TRUE & label ==TRUE) {
+      
+      
       cat(sprintf("Press enter the label of %s functional probes (eg:Y/N): ", i))
       label_func <- readline()
       
-      filename <- paste0(label_func, sprintf("_%s_Mutual_info = %s",i, round(probes$mutual[i], 3)),".png")
-      file <- paste0('./',folderpath,"/",filename)
-      png(file)
-      scatter.lin(i, mar, rna, probes)
-      dev.off()
-    }}
+      # linear probes folder
+      if(label_func == "Y"){
+        
+        lin.path <- paste0("./",folderpath, "linear_probes")
+        dir.create(lin.path, recursive = TRUE, showWarnings = FALSE)
+        filename <- paste0(label_func, sprintf("_%s_Mutual_info = %s",i, round(probes$mutual[i], 3)),".png")
+        file <- paste0('./',folderpath,"/",filename)
+        png(file)
+      }
+    } 
+    else if(press == TRUE & label ==FALSE ){
+      readline("Press Enter to continue...")
+    } else {print(paste("Processing item:", i))}
+  }
   return(fev.lst)
 }
 
-# Scatter plot for typical probes
-search.scatter <- function(mar, rna, search_gene, probes){
-  for (i in search_gene){
-    scatter(i, mar, rna, probes)
-    readline("Please enter to continue...")
-    }}
- 
-# Scatter plot
-scatter <- function(i, mar, rna, probes){
-  plot(mar[i, ], rna[i, ],
-       xlab = "Microarray log expression",
-       ylab = "RNA-seq log expression",
-       xlim = marrt.lim,
-       ylim = rseq.lim,
-       main = sprintf("Gene = %s, Mutual_info = %.3f, R^2 = %.3f",
-                      probes$genes[i], probes$mutual[i], probes$r.2[i]))
+
+
+# Only Scatter plot for special gene index and quantities
+search.scatter <- function(x, y, search_i, probes, pic_num = NULL){
+  if (is.null(pic_num)){
+    for (i in search_i){
+      scatter(i, x, y, probes)
+      readline("Please enter to continue...")
+    }
+  } 
+  else { 
+    stopifnot(pic_num > 0 & pic_num <= length(search_i))
+    for (j in 1:pic_num) {
+      scatter(j, x, y, probes)
+      readline("Please enter to continue...")
+    }
+  }
 }
 
-# Generate fev, prediction band based on linear regression and scatter plots
-lin.scatter <- function(i, mar, rna, probes) {
-  #par(mfrow=c(1,1))
-  dev.new()
-  dev.set(dev.list()[['RStudioGD']])
-  scatter(i, mar, rna,  probes)
+# Scatter plot with or without PI and predicted model according <pred>
+scatter <- function(i, x, y, probes, pred = NULL){
   
-  lin.model <- lm(rna[i, ] ~ mar[i, ])
-  abline(a = lin.model$coefficients[1], b = lin.model$coefficients[2], col = "red")
+  gene.exp <- data.frame(
+    "Microarray" = x[i, ], 
+    "RNA-seq" = y[i, ]
+    )
+  data <- gene.exp
+  
+  pic <- ggplot(data, aes(x = Microarray, y = RNA.seq)) + geom_point() + 
+    labs(x = "Microarray log expression", y = "RNA-seq log expression", 
+         title = sprintf("Gene = %s, Mutual_info = %.3f, R^2 = %.3f",
+                         probes$genes[i], probes$mutual[i], probes$r.2[i]))+
+    xlim(marrt.lim) + ylim(rseq.lim) +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  if (is.null(pred)){ print(pic) } 
+  else {
+    data <- cbind(gene.exp, pred)
+    pic <- pic + geom_ribbon(data = data,mapping = aes(ymin = lwr, ymax = upr, x = Microarray), 
+                      alpha = 0.4, inherit.aes = FALSE, fill = "#fdb338") +
+           geom_line(data = data,mapping = aes(y = fit, x = Microarray), 
+                inherit.aes = FALSE, size = 1, colour = "#025196")
+    print(pic) 
+    }
+}  
+  
 
-  fev.hat <- fev.func(rna[i, ], lin.model$fitted.values);
+# Generating linear model, CI, PI
+linear <- function(i, x, y, alpha){
   
-  idx <- order(mar[i, ]);
-  pred.ym <- predict.lm(lin.model, interval = "confidence", level = 0.95)  # confidence interval for mean of y
-  pred.yr <- predict.lm(lin.model, interval = "prediction", level = 0.95)  #
-  lines(mar[i, ][idx], pred.ym[,"lwr"][idx], col = "blue", lty = "dashed")
-  lines(mar[i, ][idx], pred.ym[,"upr"][idx], col = "blue", lty = "dashed")
-  lines(mar[i, ][idx], pred.yr[,"lwr"][idx], col = "green", lty = "dashed")
-  lines(mar[i, ][idx], pred.yr[,"upr"][idx], col = "green", lty = "dashed")
+  model <- lm(y[i, ] ~ x[i, ])
+  new <- data.frame(x[i, ])
   
-  readline("Press Enter to continue...")
-  return(fev.hat)
+  # confidence band 
+  y.hat <- predict.lm(model, newdata = new, interval = "confidence", alpha)
+  # prediction band
+  y.pred <- predict.lm(model, newdata = new, interval = "prediction", alpha)
+  
+  pred <- data.frame(
+    fit = y.hat$fit[ ,1],
+    upr = y.pred$fit[ ,2],
+    lwr = y.pred$fit[ ,3]
+  )
+  
+  output <- list()
+  output$model = model
+  output$pred = pred
+
+  return(output)
 }
+
+
 
 #Generate fev, prediction band based on scam and scatter plots
-curve.scatter <- function(mar, rna, probes, num.pic, meth = "mpi", k) {
+curve.scatter <- function(mar, rna, probes, num.pic, meth = "ts", k) {
   
   fev.lst <- c()
   if (missing(num.pic)) {num.pic = dim(mar)[1]} 
   else { 
     stopifnot(num.pic < dim(mar)[1])
-    num.pic = num.pic}
+    num.pic <- num.pic
+  }
+  coef.data <- data.frame()
+  # deriv1.d <- c()
+  # deriv1.se <- c()
+  # deriv2.d <- c()
+  # deriv2.se <- c()
+  # s.lst <- c()
+  # ho.lst <- c()
+  # ht.lst <- c()
   for (i in 1:(num.pic)) {
-    dev.new()
-    dev.set(dev.list()[['RStudioGD']])
-    scatter(i, mar, rna,  probes)
+    # dev.new()
+    # dev.set(dev.list()[['RStudioGD']])
+
     
     x1 <- mar[i, ];
     x2 <- rna[i, ];
-    fit <- scam(x2 ~ s(x1, k = k, bs = meth));
-    d <- data.frame(x1 = x1);
-    x2.hat <- predict(fit, se.fit = TRUE, d);
+    fit <- scam(x2 ~ s(x1, k = k, bs = "mpi"));
+
+    # plot(fit)
+    # scam.check(fit)
+    # summary(fit)
     
-    idx <- order(x1);
-    lines(x1[idx], x2.hat$fit[idx], lwd = 2, type = "l",col = 'red')
+# ###TODO
+#     
+#     deriv1 <- derivative.scam(fit, smooth.number=1, deriv=1)
+#     data <- data.frame(x = x1, y = x2)
+#     idx <- sort(data$x, index = TRUE)
+#     a <- data.frame(x = idx$x, y = deriv1$d[idx$ix])
+#     plot1 <- ggplot(a) + geom_line() +
+#                     aes(x = x, y = y) +  labs(title = "first derivate")
+#    
+#     #sum(deriv1$d)
+#     deriv1.d <-cbind(deriv1.d, deriv1$d)
+#     deriv1.se <- cbind(deriv1.se, deriv1$se.d)
+#     
+#     deriv2 <- derivative.scam(fit,smooth.number=1,deriv=2)
+#     xx <- sort(data$x,index=TRUE)
+#     b <- data.frame(x = xx$x, y = deriv1$d[xx$ix])
+#     # plot2 <- ggplot(b) + geom_line() +
+#     #   aes(x = x, y = y) +  labs(title = "second derivate")
+#     
+#     deriv2.d <-cbind(deriv2.d, deriv2$d)
+#     deriv2.se <- cbind(deriv2.se, deriv2$se.d)
+#     
+#     #sum(deriv2$d)
+#     # grid.arrange(plot1, plot2,  nrow = 1, ncol = 2)
+#     # picname <- paste0(i," .png")
+#     # ggsave(picname, width = 20, height = 10, dpi = 300)
+# ####TODO    
+    
+    coef.data <- rbind(coef.data,fit$coefficients)
+    
+    d <- data.frame(x1 = x1);
+    x2.hat <- predict_interval_scam(fit, d);
+    
+    # # scatter and prediction model
+    scatter(i, mar, rna, probes, x2.hat)
     
     fev.hat <- fev.func(x2, fit$fitted.values);
     fev.lst <- append(fev.lst, fev.hat)
-    readline("Press Enter to continue...")}
-  return(fev.hat)}
+    print(paste("Processing item:", i))}
+    #probes.class <- readline("Press the type of scatter trend(s/ht/ho):...")}
+    # if (probes.class == s) {s.lst = append(s.lst, i) } 
+    # else if (probes.class == ht) { ht.lst = append(ht.lst, i)} 
+    # else {ho.lst = append(ho.lst, i)}
+  return(list(fev.lst, coef.data))
+  
+}
 
 
-# Plot scatter,fitted curves,confidence band for each curve probes
-# Return FEV for each curve probes
-#  fev_info <- function(mar , rna , gene, mutual, r, func = fev, meth = "mpi", alpha){
-#   fev_lst <- c()
-#   for (i in 1:1974) {
-#     k <- 4;
-#     x1 <- mar[i, ];
-#     x2 <- rna[i, ];
-#     fit <- scam(x2 ~ s(x1, k = k, bs = meth));
-#     d <- data.frame(x1 = x1);
-#     x2.hat <- predict(fit, se.fit = TRUE, d);
-#     fev.hat <- func(x2, x2.hat$fit);
-#     fev_lst <- append(fev_lst, fev.hat)
-#     plot(x1, x2, xlim = marrt.lim, ylim = rseq.lim, 
-#          main = sprintf("Gene = %s, Mutual_info = %.3f, R^2 = %.3f", 
-#                         gene[i], mutual[i],r[i]))
-#     # fitted curve
-#     idx <- order(x1);
-#     lines(x1[idx], x2.hat$fit[idx], lwd = 2, type = "l",col = 'red')
-#     # confidence band
-#     z <- x2 - x2.hat$fit
-#     sd.pred <- (sum((z - mean(z))^2)/length(z))^(1/2)
-#     a <- qnorm(1-alpha/2, mean = mean(z), sd = sd.pred)
-#     upper <- x2.hat$fit[idx] + a
-#     lower <- x2.hat$fit[idx] - a
-#     lines(x1[idx], upper, pch=20, col = 'red' )
-#     lines(x1[idx], lower, pch=20, col = 'red')
-#     
-#     print(paste("Processing item:", i))
-#   #  Sys.sleep(1)
-#   }
-#   return(fev_lst)
-# }
+
+# predict mean with prediction interval for new data
+predict_interval_scam <- function(fit, newdata, level=0.95) {
+  y.hat <- predict(fit, se.fit=TRUE, type="response", newdata);
+  
+  # sigma2.hat is based on old data (1..N)
+  nu <- length(fit$residuals) - length(coef(fit));
+  sigma2.hat <- 1 / nu * sum( fit$residuals^2 );
+  
+  # \sqrt( \hat{ var[w] } ) = 
+  #   \sqrt( \hat{ var[y_{N+1}] } + \hat{ var[\hat{y_{N+1}}] }
+  sqrt_hat_var_w <- sqrt(sigma2.hat + y.hat$se.fit^2);
+  
+  tq <- qt(1 - (1 - level)/2, df=nu);
+  se <- tq*sqrt_hat_var_w;
+  pred <- data.frame(
+    fit = y.hat$fit,
+    se.fit = y.hat$se.fit,
+    lwr = y.hat$fit - se,
+    upr = y.hat$fit + se
+  )
+  return(pred)
+  }
